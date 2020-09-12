af6c21909ea05ed9057ff55148b46fb5f0c6c31e
Michel Nickbon <michel.lemponen@gmail.com>
2020-09-12 20:20:24 +0200

    Added loadCompany function

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,20 +1,21 @@
 <template>
   <div>
-  <v-autocomplete
-    v-model="select"
-    :loading="loading"
-    :items="availableCompanies"
-    :item-value="'id'"
-    :item-text="'name'"
-    :search-input.sync="search"
-    cache-items
-    class="mx-4"
-    flat
-    hide-no-data
-    hide-details
-    label="Search for a publicly listed company"
-    solo-inverted
-  ></v-autocomplete>
+    <v-autocomplete
+      @change="loadCompany"
+      v-model="select"
+      :loading="loading"
+      :items="availableCompanies"
+      :item-value="'id'"
+      :item-text="'name'"
+      :search-input.sync="search"
+      cache-items
+      class="mx-4"
+      flat
+      hide-no-data
+      hide-details
+      label="Search for a publicly listed company"
+      solo-inverted
+    ></v-autocomplete>
   </div>
 </template>
 
@@ -26,23 +27,11 @@ export default {
   data () {
       return {
         loading: false,
-        items: [],
         search: null,
         select: null,
-        states: [
-          'Alabama',
-          'Alaska',
-          'American Samoa',
-          'Arizona',
-          'Arkansas',
-          'California',
-          'Colorado',
-          'Connecticut',
-        ],
         availableCompanies: []
       }
     },
-
     async mounted() {
       try {
         const result = await Base.GetData("/companies/GetCompanyList");
@@ -54,6 +43,17 @@ export default {
         });
       } catch (error) {
         console.log("Error while fetching data", error);
+      }
+    },
+    methods: {
+      async loadCompany(companyId) {
+        if (companyId) {
+          try {
+            const result = await Base.GetData("/companies/" + companyId);
+          } catch (error) {
+            console.log("Error while fetching data", error);
+          }
+        }
       }
     }
 }
